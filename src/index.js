bf1e2c7bd375ed302dff1aaf30b1b97500c6621a
Kira Miller <kira.miller2011@gmail.com>
2019-07-24 15:32:34 -0700

    ch3.1 changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-var style = {
-    backgroundColor: 'orange',
-    color: 'white',
-    fontFamily: 'Arial'
+
+class Message extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hi everyone!</h1>
+            </div>
+        )
+    }
 }
 
-// JSX tags included:
-ReactDOM.render(
-    <div style={style}>
-        <h1 id="heading-element">Hello World</h1>
-        <p>We are glad you are here</p>
-    </div>,
-    document.getElementById('root')
-)
+ReactDOM.render(<Message />, document.getElementById('root'))
