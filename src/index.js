6168bfd1c0b16527b0ccb870c492dff93f6a5ebb
Kira Miller <kira.miller2011@gmail.com>
2019-07-26 14:09:51 -0700

    ch3.2 changes

--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ import ReactDOM from 'react-dom'
 
 class Message extends React.Component {
     render() {
+        console.log(this.props)
         return (
             <div>
-                <h1>Hi everyone!</h1>
+                <h1 style={{color: this.props.color}}>
+                    {this.props.msg}
+                </h1>
+                <p>I'll check back in {this.props.minutes} minutes</p>
             </div>
         )
     }
 }
 
-ReactDOM.render(<Message />, document.getElementById('root'))
+ReactDOM.render(
+    <Message age={50} color="blue" msg="how are you?" minutes={5} />,
+    document.getElementById('root')
+)
